edb0fb8d202fbe0419ed988287bd333b2c9af86f
Drwheats <Dr.Wheats@gmail.com>
2022-11-16 18:35:25 -0500

    yeet

--- src/App.js
+++ src/App.js
@@ -2,35 +2,100 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import mineLogo from './media/minepumpkin.png';
 import mineBad from './media/mine.png'
+import { Router } from 'react-router';
 
 function App() {
   const [grid, setGrid] = useState(0);
   const [mines, setMines] = useState(0);
+  const [gameState, setGameState] = useState(true);
 
   function showFormIfInputGood() {
     document.getElementById("form3").style.visibility = "visible";
     document.getElementById("form4").style.visibility = "visible";
+    document.getElementById("errormessage").style.visibility = "hidden";
+
   }
   function hideFormIfInputBad() {
     document.getElementById("form3").style.visibility = "hidden";
     document.getElementById("form4").style.visibility = "hidden";
+    document.getElementById("errormessage").style.visibility = "visible";
 
+    
   }
   function clickSpawn() {
     prompt("There are " + grid + "x" + grid + " spaces and there are " + mines + " mines.")
+    setGameState = true;
   } // This will be used to run the program, right now it is reacting for testing purposes.
 
   useEffect(() => {
-    if (grid > 1 && grid < 70 && (grid * grid) > mines) {
+    if (grid > 1 && grid < 70 && (grid * grid) > mines && gameState) {
       showFormIfInputGood();
     }
     else {
       hideFormIfInputBad();
     }
   })
+  function GameGrid(props) {
+      const row_length = props.gridnum;
+      const num_grid = row_length * row_length;
+      const mines = props.minenum;
+      let gameGrid = [];
+      const gameColumns = [];
+      let c = 0;
+      const mineArray = [];
+      const scoreToWin = num_grid; 
+      
+      const handleClick = (event, key) => {
+        console.log('this is ' + event.target.className + ' number: ', key);
+        if (event.target.className === "yesMine") {
+          console.log('MINE');
+          setGameState(false);
+        }
+        if (event.target.className === "notMine") {
+          console.log('NOT');
+        }
+      };
+      
+    
+      // makes array of non-mine objects
+      for (let i=0; i < num_grid; i++) {
+        gameGrid.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="notMine" src={mineLogo}></img>)
+      }
+    
+      // makes array of mine objects
+      for (let i=0; i < mines; i++) {
+        mineArray.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="yesMine" src={mineBad}></img>)
+      }
+    
+      // next 2 blocks get rid of x non-mine objects and add the whole mine array into the grid.
+      for (let i = 0; i < mines; i++) {
+        gameGrid.pop();
+      }
+    
+      for (let i = 0; i < mines; i++) {
+        gameGrid.push(mineArray[i]);
+      }
+      gameGrid = gameGrid.map(value => ({value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({value}) => value)
+      // Randomizes the board.
+    
+      //takes from the mineGrid array of grid objects, makes them into rows, then makes X rows to make the whole board
+      for (let j=0; j < row_length; j++) {
+        let mineRows = [];
+        for (let i=0; i < row_length; i++) {
+          // If there is a time where I need to give every object in mineGrid a unique key, this is where I would do it by cloning the object.
+          mineRows.push(gameGrid[c])
+          c += 1;
+        }
+        gameColumns.push(mineRows)
+        gameColumns.push(<br></br>)
+      } // defines the good spaces on the grid
+    
+      // renders grid for gamegrid object
+      return (gameColumns);
+      }
     
   return (
-    <div className="App">Le App
+    <div className="App">Mine Sweeping Simulator for COMP1012
       <header className="App-header">
           <p className='form1'> 
           <label>Number of Boxes: </label>
@@ -46,61 +111,71 @@ function App() {
           </p>
         <p className='form4' id='form4'>
         <GameGrid gridnum={grid} minenum={mines}/></p>
+        <p className='errormessage' id='errormessage'>Invalid Settings; Please Try Again.</p>
       </header>
     </div>
   );
 }
 
-const handleClick = (event, key) => {
-  console.log();
-  console.log('this is ' + event.target.className + ' number: ', key);
-};
 
-function GameGrid(props) {
-  const row_length = props.gridnum;
-  const num_grid = row_length * row_length;
-  const mines = props.minenum;
-  let gameGrid = [];
-  const gameColumns = [];
-  let c = 0;
-  const mineArray = []; 
+// function GameGrid(props) {
+//   const row_length = props.gridnum;
+//   const num_grid = row_length * row_length;
+//   const mines = props.minenum;
+//   let gameGrid = [];
+//   const gameColumns = [];
+//   let c = 0;
+//   const mineArray = [];
+//   const scoreToWin = num_grid; 
   
-  // makes array of non-mine objects
-  for (let i=0; i < num_grid; i++) {
-    gameGrid.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="notMine" src={mineLogo}></img>)
-  }
+//   const handleClick = (event, key) => {
+//     console.log('this is ' + event.target.className + ' number: ', key);
+//     if (event.target.className === "yesMine") {
+//       console.log('MINE');
+//       setGameState(false);
+//     }
+//     if (event.target.className === "notMine") {
+//       console.log('NOT');
+//     }
+//   };
+  
 
-  // makes array of mine objects
-  for (let i=0; i < mines; i++) {
-    mineArray.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="yesMine" src={mineBad}></img>)
-  }
+//   // makes array of non-mine objects
+//   for (let i=0; i < num_grid; i++) {
+//     gameGrid.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="notMine" src={mineLogo}></img>)
+//   }
 
-  // next 2 blocks get rid of x non-mine objects and add the whole mine array into the grid.
-  for (let i = 0; i < mines; i++) {
-    gameGrid.pop();
-  }
+//   // makes array of mine objects
+//   for (let i=0; i < mines; i++) {
+//     mineArray.push(<img alt="mine?" onClick={e => handleClick(e, i)} className="yesMine" src={mineBad}></img>)
+//   }
 
-  for (let i = 0; i < mines; i++) {
-    gameGrid.push(mineArray[i]);
-  }
-  gameGrid = gameGrid.map(value => ({value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({value}) => value)
-  // Randomizes the board.
+//   // next 2 blocks get rid of x non-mine objects and add the whole mine array into the grid.
+//   for (let i = 0; i < mines; i++) {
+//     gameGrid.pop();
+//   }
 
-  //takes from the mineGrid array of grid objects, makes them into rows, then makes X rows to make the whole board
-  for (let j=0; j < row_length; j++) {
-    let mineRows = [];
-    for (let i=0; i < row_length; i++) {
-      // If there is a time where I need to give every object in mineGrid a unique key, this is where I would do it by cloning the object.
-      mineRows.push(gameGrid[c])
-      c += 1;
-    }
-    gameColumns.push(mineRows)
-    gameColumns.push(<br></br>)
-  } // defines the good spaces on the grid
+//   for (let i = 0; i < mines; i++) {
+//     gameGrid.push(mineArray[i]);
+//   }
+//   gameGrid = gameGrid.map(value => ({value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({value}) => value)
+//   // Randomizes the board.
 
-  // renders grid for gamegrid object
-  return (gameColumns);
-  }
+//   //takes from the mineGrid array of grid objects, makes them into rows, then makes X rows to make the whole board
+//   for (let j=0; j < row_length; j++) {
+//     let mineRows = [];
+//     for (let i=0; i < row_length; i++) {
+//       // If there is a time where I need to give every object in mineGrid a unique key, this is where I would do it by cloning the object.
+//       mineRows.push(gameGrid[c])
+//       c += 1;
+//     }
+//     gameColumns.push(mineRows)
+//     gameColumns.push(<br></br>)
+//   } // defines the good spaces on the grid
+
+//   // renders grid for gamegrid object
+//   return (gameColumns);
+//   }
 
 
 
